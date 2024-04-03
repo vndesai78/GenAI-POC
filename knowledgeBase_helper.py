3d15a7f2ef73b1597b0b9cef4680a974c617e52e
vndesai78 <164612277+vndesai78@users.noreply.github.com>
2024-04-03 18:11:03 +0530

    Update knowledgeBase_helper.py

--- knowledgeBase_helper.py
+++ knowledgeBase_helper.py
@@ -6,7 +6,7 @@ pp = pprint.PrettyPrinter(indent=2)
 
 bedrock_config = Config(connect_timeout=120, read_timeout=120, retries={'max_attempts': 0})
 bedrock_client = boto3.client('bedrock-runtime', region_name='us-east-1')
-bedrock_agent_client = boto3.client("bedrock-agent-runtime",config=bedrock_config)
+bedrock_agent_client = boto3.client("bedrock-agent-runtime",config=bedrock_config, region_name='us-east-1')
 #bedrock_agent_client.synch()
 
 #model_id = "anthropic.claude-instant-v1" # try with both claude instant as well as claude-v2. for claude v2 - "anthropic.claude-v2"
