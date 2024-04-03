d5b3a640142a2384d87b0ee47eb4da1d6bd81499
vndesai78 <164612277+vndesai78@users.noreply.github.com>
2024-04-03 18:14:44 +0530

    Update ActPlanKnowledgeBase.py

--- ActPlanKnowledgeBase.py
+++ ActPlanKnowledgeBase.py
@@ -5,8 +5,8 @@ from botocore.client import Config
 pp = pprint.PrettyPrinter(indent=2)
 
 bedrock_config = Config(connect_timeout=120, read_timeout=120, retries={'max_attempts': 0})
-bedrock_client = boto3.client('bedrock-runtime')
-bedrock_agent_client = boto3.client("bedrock-agent-runtime",config=bedrock_config)
+bedrock_client = boto3.client('bedrock-runtime', region_name='us-east-1')
+bedrock_agent_client = boto3.client("bedrock-agent-runtime",config=bedrock_config, region_name='us-east-1')
 
 #model_id = "anthropic.claude-instant-v1" # try with both claude instant as well as claude-v2. for claude v2 - "anthropic.claude-v2"
 modelid = "anthropic.claude-v2:1"
